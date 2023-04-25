b14e193aa56d1f3aa3729a517d98011186662015
Anastasiia Savenok <i.wasber@gmail.com>
2023-04-25 13:44:54 +0300

    create new structure folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
-import Profile from './Profile';
-import Statistics from './Statictics';
-import FriendList from './FriendList';
-import TransactionHistory from './TransactionHistory';
-import user from '../user.json';
-import data from '../data.json';
-import friends from '../friends.json';
-import transactions from '../transactions.json';
+import Profile from './Profile/Profile';
+import Statistics from './Statictics/Statictics';
+import FriendList from './FriendsList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
 export default function App() {
   return (
