448607eb77db2db1622400c59ebce9a40a1fbed3
Anastasiia Savenok <i.wasber@gmail.com>
2023-04-25 13:26:28 +0300

    add transaction markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import Profile from './Profile';
 import Statistics from './Statictics';
 import FriendList from './FriendList';
+import TransactionHistory from './TransactionHistory';
 import user from '../user.json';
 import data from '../data.json';
 import friends from '../friends.json';
@@ -17,8 +18,8 @@ export default function App() {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />;
-      <TransactionHistory items={transactions} />;
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 }
