5844beb74e6cadc07b8944f0460ca89aaf5a60b7
Anastasiia Savenok <i.wasber@gmail.com>
2023-04-25 13:03:43 +0300

    create friends markup

--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -1,6 +1,18 @@
 import PropTypes from 'prop-types';
+import FriendListItem from './FriendListItem';
 const FriendList = ({ friends }) => {
-  return <ul class="friend-list"></ul>;
+  return (
+    <ul class="friend-list">
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
 };
 
 FriendList.propTypes = {
