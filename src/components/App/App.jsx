931b2a7361052f1ceb44a5aac4b00b23d63a794f
Anastasiia Savenok <i.wasber@gmail.com>
2023-04-25 13:56:06 +0300

    change imports

---
+++ src/components/App/App.jsx
@@ -0,0 +1,25 @@
+import Profile from '../Profile/Profile';
+import Statistics from '../Statictics/Statictics';
+import FriendList from '../FriendsList/FriendList';
+import TransactionHistory from '../TransactionHistory/TransactionHistory';
+import user from '../../data/user';
+import data from '../../data/data';
+import friends from '../../data/friends';
+import transactions from '../../data/transactions';
+
+export default function App() {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+}
